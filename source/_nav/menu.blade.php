27eeec1b981f278a858e567091a4ddd9b3d6d605
David Hallin <hallin.david@gmail.com>
2020-08-05 06:45:12 -0400

    Updating to add about section

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -11,4 +11,5 @@
 
     <a title="Dave's Twitter" target="_blank()" class="ml-6 text-gray-700 hover:text-blue-600" href="https://twitter.com/hallindavid"><i class="fab fa-twitter"></i></a>
     <a title="Dave's GitHub" target="_blank()" class="ml-6 text-gray-700 hover:text-blue-600" href="https://github.com/hallindavid"><i class="fab fa-github"></i></a>
+    <a title="Dave's StackShare" target="_blank()" class="ml-6 items-center" href="https://stackshare.io/hallindavid/ihaveused"><img src="/assets/img/stackshare.png" class="h-4 w-4 hover:opacity-75"></a>
 </nav>
